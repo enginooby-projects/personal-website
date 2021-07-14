9b7dcf098cbb8f667848d3df4659627ec1d331db
unknown <enginoobz@gmail.com>
2021-07-14 11:57:43 +0200

    Dynamic FLAT style using CSSStyleRule

--- built/FlatStyle.js
+++ built/FlatStyle.js
@@ -1,23 +1,12 @@
 import * as DynamicTheme from './DynamicTheme.js';
 import { Style } from './Style.js';
-import { StyleRuleStore } from './StyleRuleStore.js';
-const backgroundHighlightColorSelectors = formatString([
-    ".radio-selection",
-    ".portfolio-filter .pill-button.active",
-    ".pill-button",
-    " .theme-skin .pill-button.active",
-    ".theme-skin .pill-button:not(.active):hover",
-    ".portfolio-filter .pill-button:not(.active):hover",
-    "table>tbody>tr:hover",
-    "::-webkit-scrollbar-thumb",
-    "::-webkit-slider-thumb "
-]);
-const backgroundDarkenHighlightColorSelectors = [
-    "::-webkit-scrollbar-thumb:hover",
-    "::-webkit-slider-thumb:hover",
-    ".pill-button:hover"
+const bgSchemeSelectors = [
+    ".display-content>.container",
+    ".range-slider__range",
+    ".range-slider__value",
+    ".modal-content"
 ];
-const backgroundLightenSchemeColorSelectors = formatString([
+const bgLightenSchemeSelectors = [
     ".box-border",
     ".image-border",
     ".contact .form-item .form-group",
@@ -27,42 +16,59 @@ const backgroundLightenSchemeColorSelectors = formatString([
     ".color-pallet",
     ".pallet-button",
     "::-webkit-scrollbar-track"
-]);
-const backgroundSchemeColorSelectors = formatString([
-    ".display-content>.container",
-    ".range-slider__range",
-    ".range-slider__value",
-]);
-const colorBaseColorSelectors = formatString([
-    ".portfolio-filter .pill-button ",
-]);
-const colorHighlightColorSelectors = [
+];
+const bgHighlightSelectors = [
+    ".radio-selection",
+    ".portfolio-filter .pill-button.active",
+    ".pill-button:not( .theme-skin .pill-button):not(.portfolio-filter .pill-button)",
+    " .theme-skin .pill-button.active",
+    ".theme-skin .pill-button:not(.active):hover",
+    ".portfolio-filter .pill-button:not(.active):hover",
+    "table>tbody>tr:hover",
+    "::-webkit-scrollbar-thumb",
+    "::-webkit-slider-thumb "
+];
+const bgDarkenHighlightSelectors = [
+    "::-webkit-scrollbar-thumb:hover",
+    "::-webkit-slider-thumb:hover",
+    ".pill-button:hover"
+];
+const colorHighlightSelectors = [
     ".segmented-control>input:hover+label",
     ".portfolio-filter .pill-button:not(.active)",
     ".theme-skin .pill-button:not(.active)",
-    ".checkbox input:hover~label",
+    ".checkbox input:hover~label i",
     ".checkbox input:checked~label i",
     ".checkbox input:checked~label+.name"
 ];
-const colorMutedBaseColorSelectors = [
+const colorContrastHighlightSelectors = [
+    ".segmented-control>input:checked+label",
+];
+const colorBaseSelectors = [
+    ".portfolio-filter .pill-button ",
+];
+const colorMutedBaseSelectors = [
     ".segmented-control>input:not(:checked):not(:hover)+label",
     ".checkbox input:not(:checked)~label+.name",
     ".checkbox input:not(:checked):not(:hover)~label i",
 ];
-const colorContrastHighlightColorSelectors = [
-    ".segmented-control>input:checked+label",
-];
-function formatString(selectorsArray) {
-    return selectorsArray.join(", ");
-}
 export class FlatStyle extends Style {
     constructor() {
         super();
         this.lightSchemeIntensity = 5;
-        this.lightenSchemeColor = "#e1e1e1";
         // darkenSchemeColor: string = "#c7c7c7";
         this.darkHighlightIntensity = 15;
+        this.lightenSchemeColor = "#e1e1e1";
         this.darkenHighlightColor = "#033669";
+        // lazy initializations
+        this.getBgSchemeRule = () => { var _a; return (_a = this.bgSchemeRule) !== null && _a !== void 0 ? _a : (this.bgSchemeRule = this.insertEmptyRule(bgSchemeSelectors)); };
+        this.getBgLightenSchemeRule = () => { var _a; return (_a = this.bgLightenSchemeRule) !== null && _a !== void 0 ? _a : (this.bgLightenSchemeRule = this.insertEmptyRule(bgLightenSchemeSelectors)); };
+        this.getBgHighlightRule = () => { var _a; return (_a = this.bgHighlightRule) !== null && _a !== void 0 ? _a : (this.bgHighlightRule = this.insertEmptyRule(bgHighlightSelectors)); };
+        this.getBgDarkenHighlightRule = () => { var _a; return (_a = this.bgDarkenHighlightRule) !== null && _a !== void 0 ? _a : (this.bgDarkenHighlightRule = this.insertEmptyRule(bgDarkenHighlightSelectors)); };
+        this.getColorHighlightRule = () => { var _a; return (_a = this.colorHighlightRule) !== null && _a !== void 0 ? _a : (this.colorHighlightRule = this.insertEmptyRule(colorHighlightSelectors)); };
+        this.getColorContrastHighlightRule = () => { var _a; return (_a = this.colorContrastHighlightRule) !== null && _a !== void 0 ? _a : (this.colorContrastHighlightRule = this.insertEmptyRule(colorContrastHighlightSelectors)); };
+        this.getColorBaseRule = () => { var _a; return (_a = this.colorBaseRule) !== null && _a !== void 0 ? _a : (this.colorBaseRule = this.insertEmptyRule(colorBaseSelectors)); };
+        this.getColorMutedBaseRule = () => { var _a; return (_a = this.colorMutedBaseRule) !== null && _a !== void 0 ? _a : (this.colorMutedBaseRule = this.insertEmptyRule(colorMutedBaseSelectors)); };
     }
     static get Instance() {
         var _a;
@@ -71,24 +77,26 @@ export class FlatStyle extends Style {
     }
     init() {
         $("body").addClass('flat-style');
-        console.log('<<<<<<<<<<<');
     }
     setupCustomizeEvents() { }
     onHighlightColorUpdated() {
-        $(backgroundHighlightColorSelectors).css('background-color', DynamicTheme.highlightColor.hex);
-        $(":not(.portfolio-filter) .pill-button").css('color', DynamicTheme.highlightColor.getInvertBlackWhite());
-        $(".theme-skin .pill-button.active").css('background-color', DynamicTheme.highlightColor.getDarken(15));
-        StyleRuleStore.Instance.getSliderThumbHoverRule().style.backgroundColor = DynamicTheme.highlightColor.hex;
+        this.darkenHighlightColor = DynamicTheme.highlightColor.getDarken(this.darkHighlightIntensity);
+        this.getBgHighlightRule().style.setProperty('background-color', DynamicTheme.highlightColor.hex, 'important');
+        this.getBgHighlightRule().style.setProperty('color', DynamicTheme.highlightColor.getInvertBlackWhite(), 'important');
+        this.getBgDarkenHighlightRule().style.setProperty('background-color', this.darkenHighlightColor, 'important');
+        this.getBgDarkenHighlightRule().style.setProperty('color', DynamicTheme.highlightColor.getInvertBlackWhite(), 'important');
+        this.getColorHighlightRule().style.setProperty('color', DynamicTheme.highlightColor.hex, 'important');
+        this.getColorContrastHighlightRule().style.setProperty('color', DynamicTheme.highlightColor.getInvertBlackWhite(), 'important');
     }
     onSchemeColorUpdated() {
         this.lightenSchemeColor = DynamicTheme.schemeColor.getLighten(this.lightSchemeIntensity);
-        $(backgroundLightenSchemeColorSelectors).css('background-color', this.lightenSchemeColor);
-        // $(backgroundDarkenSchemeColorSelectors).css('background-color', this.darkenSchemeColor);
-        $(backgroundSchemeColorSelectors).css('background-color', DynamicTheme.schemeColor.hex);
-        $(colorBaseColorSelectors).css('color', DynamicTheme.baseColor);
-        StyleRuleStore.Instance.getTrackScrollbarRule().style.background = this.lightenSchemeColor;
-        // StyleRuleStore.Instance.getThumbScrollbarRule().style.background = this.darkenSchemeColor;
+        this.getBgSchemeRule().style.setProperty('background-color', DynamicTheme.schemeColor.hex, 'important');
+        this.getBgSchemeRule().style.setProperty('color', DynamicTheme.schemeColor.getInvertBlackWhite(), 'important');
+        this.getBgLightenSchemeRule().style.setProperty('background-color', this.lightenSchemeColor, 'important');
+        this.getBgLightenSchemeRule().style.setProperty('color', DynamicTheme.schemeColor.getInvertBlackWhite(), 'important');
     }
     onBaseColorUpdated() {
+        this.getColorBaseRule().style.setProperty('color', DynamicTheme.baseColor, 'important');
+        this.getColorMutedBaseRule().style.setProperty('color', DynamicTheme.mutedBaseColor, 'important');
     }
 }
--- src/FlatStyle.ts
+++ src/FlatStyle.ts
@@ -1,26 +1,14 @@
 import * as DynamicTheme from './DynamicTheme.js';
 import { Style } from './Style.js';
-import { StyleRuleStore } from './StyleRuleStore.js';
 
-const backgroundHighlightColorSelectors = formatString([//colorContrastHighlightColorSelectors
-        ".radio-selection",
-        ".portfolio-filter .pill-button.active",
-        ".pill-button",
-        " .theme-skin .pill-button.active",
-        ".theme-skin .pill-button:not(.active):hover",
-        ".portfolio-filter .pill-button:not(.active):hover",
-        "table>tbody>tr:hover",
-        "::-webkit-scrollbar-thumb",
-        "::-webkit-slider-thumb "
-]);
+const bgSchemeSelectors = [
+        ".display-content>.container",
+        ".range-slider__range",
+        ".range-slider__value",
+        ".modal-content"
+];
 
-const backgroundDarkenHighlightColorSelectors = [
-        "::-webkit-scrollbar-thumb:hover",
-        "::-webkit-slider-thumb:hover",
-        ".pill-button:hover"
-]
-
-const backgroundLightenSchemeColorSelectors = formatString([ //colorBaseColorSelectors
+const bgLightenSchemeSelectors = [ //colorBaseColorSelectors
         ".box-border",
         ".image-border",
         ".contact .form-item .form-group",
@@ -30,41 +18,48 @@ const backgroundLightenSchemeColorSelectors = formatString([ //colorBaseColorSel
         ".color-pallet",
         ".pallet-button",
         "::-webkit-scrollbar-track"
-]);
+];
 
-const backgroundSchemeColorSelectors = formatString([
-        ".display-content>.container",
-        ".range-slider__range",
-        ".range-slider__value",
-]);
+const bgHighlightSelectors = [//colorContrastHighlightColorSelectors
+        ".radio-selection",
+        ".portfolio-filter .pill-button.active",
+        ".pill-button:not( .theme-skin .pill-button):not(.portfolio-filter .pill-button)",
+        " .theme-skin .pill-button.active",
+        ".theme-skin .pill-button:not(.active):hover",
+        ".portfolio-filter .pill-button:not(.active):hover",
+        "table>tbody>tr:hover",
+        "::-webkit-scrollbar-thumb",
+        "::-webkit-slider-thumb "
+];
 
-const colorBaseColorSelectors = formatString([
-        ".portfolio-filter .pill-button ",
-]);
+const bgDarkenHighlightSelectors = [
+        "::-webkit-scrollbar-thumb:hover",
+        "::-webkit-slider-thumb:hover",
+        ".pill-button:hover"
+]
 
-const colorHighlightColorSelectors = [
+const colorHighlightSelectors = [
         ".segmented-control>input:hover+label",
         ".portfolio-filter .pill-button:not(.active)",
         ".theme-skin .pill-button:not(.active)",
-        ".checkbox input:hover~label",
+        ".checkbox input:hover~label i",
         ".checkbox input:checked~label i",
         ".checkbox input:checked~label+.name"
 ];
 
-const colorMutedBaseColorSelectors = [
+const colorContrastHighlightSelectors = [
+        ".segmented-control>input:checked+label",
+]
+
+const colorBaseSelectors = [
+        ".portfolio-filter .pill-button ",
+];
+
+const colorMutedBaseSelectors = [
         ".segmented-control>input:not(:checked):not(:hover)+label",
         ".checkbox input:not(:checked)~label+.name",
         ".checkbox input:not(:checked):not(:hover)~label i",
 ]
-
-const colorContrastHighlightColorSelectors = [
-        ".segmented-control>input:checked+label",
-]
-
-
-function formatString(selectorsArray: string[]): string {
-        return selectorsArray.join(", ");
-}
 
 export class FlatStyle extends Style {
         // Singleton Pattern
@@ -76,35 +71,56 @@ export class FlatStyle extends Style {
         }
 
         lightSchemeIntensity: number = 5;
-        lightenSchemeColor: string = "#e1e1e1";
         // darkenSchemeColor: string = "#c7c7c7";
         darkHighlightIntensity: number = 15;
+        lightenSchemeColor: string = "#e1e1e1";
         darkenHighlightColor: string = "#033669"
+
+        private bgSchemeRule?: CSSStyleRule;
+        private bgLightenSchemeRule?: CSSStyleRule;
+        private bgHighlightRule?: CSSStyleRule;
+        private bgDarkenHighlightRule?: CSSStyleRule;
+        private colorHighlightRule?: CSSStyleRule;
+        private colorContrastHighlightRule?: CSSStyleRule;
+        private colorBaseRule?: CSSStyleRule;
+        private colorMutedBaseRule?: CSSStyleRule;
+
+        // lazy initializations
+        getBgSchemeRule = () => this.bgSchemeRule ?? (this.bgSchemeRule = this.insertEmptyRule(bgSchemeSelectors));
+        getBgLightenSchemeRule = () => this.bgLightenSchemeRule ?? (this.bgLightenSchemeRule = this.insertEmptyRule(bgLightenSchemeSelectors));
+        getBgHighlightRule = () => this.bgHighlightRule ?? (this.bgHighlightRule = this.insertEmptyRule(bgHighlightSelectors));
+        getBgDarkenHighlightRule = () => this.bgDarkenHighlightRule ?? (this.bgDarkenHighlightRule = this.insertEmptyRule(bgDarkenHighlightSelectors));
+        getColorHighlightRule = () => this.colorHighlightRule ?? (this.colorHighlightRule = this.insertEmptyRule(colorHighlightSelectors));
+        getColorContrastHighlightRule = () => this.colorContrastHighlightRule ?? (this.colorContrastHighlightRule = this.insertEmptyRule(colorContrastHighlightSelectors));
+        getColorBaseRule = () => this.colorBaseRule ?? (this.colorBaseRule = this.insertEmptyRule(colorBaseSelectors));
+        getColorMutedBaseRule = () => this.colorMutedBaseRule ?? (this.colorMutedBaseRule = this.insertEmptyRule(colorMutedBaseSelectors));
 
         init() {
                 $("body").addClass('flat-style');
-                console.log('<<<<<<<<<<<');
         }
 
         setupCustomizeEvents(): void { }
 
         onHighlightColorUpdated(): void {
-                $(backgroundHighlightColorSelectors).css('background-color', DynamicTheme.highlightColor.hex);
-                $(":not(.portfolio-filter) .pill-button").css('color', DynamicTheme.highlightColor.getInvertBlackWhite());
-                $(".theme-skin .pill-button.active").css('background-color', DynamicTheme.highlightColor.getDarken(15));
-                StyleRuleStore.Instance.getSliderThumbHoverRule().style.backgroundColor = DynamicTheme.highlightColor.hex;
+                this.darkenHighlightColor = DynamicTheme.highlightColor.getDarken(this.darkHighlightIntensity);
+                this.getBgHighlightRule().style.setProperty('background-color', DynamicTheme.highlightColor.hex, 'important');
+                this.getBgHighlightRule().style.setProperty('color', DynamicTheme.highlightColor.getInvertBlackWhite(), 'important');
+                this.getBgDarkenHighlightRule().style.setProperty('background-color', this.darkenHighlightColor, 'important');
+                this.getBgDarkenHighlightRule().style.setProperty('color', DynamicTheme.highlightColor.getInvertBlackWhite(), 'important');
+                this.getColorHighlightRule().style.setProperty('color', DynamicTheme.highlightColor.hex, 'important');
+                this.getColorContrastHighlightRule().style.setProperty('color', DynamicTheme.highlightColor.getInvertBlackWhite(), 'important');
         }
 
         onSchemeColorUpdated(): void {
                 this.lightenSchemeColor = DynamicTheme.schemeColor.getLighten(this.lightSchemeIntensity);
-                $(backgroundLightenSchemeColorSelectors).css('background-color', this.lightenSchemeColor);
-                // $(backgroundDarkenSchemeColorSelectors).css('background-color', this.darkenSchemeColor);
-                $(backgroundSchemeColorSelectors).css('background-color', DynamicTheme.schemeColor.hex);
-                $(colorBaseColorSelectors).css('color', DynamicTheme.baseColor);
-                StyleRuleStore.Instance.getTrackScrollbarRule().style.background = this.lightenSchemeColor;
-                // StyleRuleStore.Instance.getThumbScrollbarRule().style.background = this.darkenSchemeColor;
+                this.getBgSchemeRule().style.setProperty('background-color', DynamicTheme.schemeColor.hex, 'important');
+                this.getBgSchemeRule().style.setProperty('color', DynamicTheme.schemeColor.getInvertBlackWhite(), 'important');
+                this.getBgLightenSchemeRule().style.setProperty('background-color', this.lightenSchemeColor, 'important');
+                this.getBgLightenSchemeRule().style.setProperty('color', DynamicTheme.schemeColor.getInvertBlackWhite(), 'important');
         }
 
         onBaseColorUpdated(): void {
+                this.getColorBaseRule().style.setProperty('color', DynamicTheme.baseColor, 'important');
+                this.getColorMutedBaseRule().style.setProperty('color', DynamicTheme.mutedBaseColor, 'important');
         }
 }
