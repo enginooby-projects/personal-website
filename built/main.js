5048821933e59518e4f75d608c9789400767c5a2
unknown <enginoobz@gmail.com>
2021-07-17 03:38:22 +0200

    Use passive listeners for jQuery

--- built/main.js
+++ built/main.js
@@ -19,6 +19,7 @@ $(document).ready(function () {
     setupModalEvents();
     return $('[data-toggle="tooltip"]').tooltip();
 });
+fixJqueryPassiveListeners();
 setupLazyLoading();
 $window.on("load", (function () {
     $("#overlayer").delay(500).fadeOut('slow');
@@ -68,6 +69,36 @@ function setupLazyLoading() {
             imageObserver.observe(v);
         });
     });
+}
+function fixJqueryPassiveListeners() {
+    jQuery.event.special.touchstart = {
+        setup: function (_, ns, handle) {
+            this.addEventListener("touchstart", handle, {
+                passive: !ns.includes("noPreventDefault")
+            });
+        }
+    };
+    jQuery.event.special.touchmove = {
+        setup: function (_, ns, handle) {
+            this.addEventListener("touchmove", handle, {
+                passive: !ns.includes("noPreventDefault")
+            });
+        }
+    };
+    jQuery.event.special.wheel = {
+        setup: function (_, ns, handle) {
+            this.addEventListener("wheel", handle, {
+                passive: true
+            });
+        }
+    };
+    jQuery.event.special.mousewheel = {
+        setup: function (_, ns, handle) {
+            this.addEventListener("mousewheel", handle, {
+                passive: true
+            });
+        }
+    };
 }
 /*-------------------------
   Testimonial CAROUSEL JS
--- src/main.js
+++ src/main.js
@@ -25,6 +25,7 @@ $(document).ready(function () {
         return $('[data-toggle="tooltip"]').tooltip();
 });
 
+fixJqueryPassiveListeners();
 setupLazyLoading();
 
 $window.on("load", (function () {
@@ -80,6 +81,37 @@ function setupLazyLoading() {
         })
 }
 
+
+function fixJqueryPassiveListeners() {
+        jQuery.event.special.touchstart = {
+                setup: function (_, ns, handle) {
+                        this.addEventListener("touchstart", handle, {
+                                passive: !ns.includes("noPreventDefault")
+                        });
+                }
+        };
+        jQuery.event.special.touchmove = {
+                setup: function (_, ns, handle) {
+                        this.addEventListener("touchmove", handle, {
+                                passive: !ns.includes("noPreventDefault")
+                        });
+                }
+        };
+        jQuery.event.special.wheel = {
+                setup: function (_, ns, handle) {
+                        this.addEventListener("wheel", handle, {
+                                passive: true
+                        });
+                }
+        };
+        jQuery.event.special.mousewheel = {
+                setup: function (_, ns, handle) {
+                        this.addEventListener("mousewheel", handle, {
+                                passive: true
+                        });
+                }
+        };
+}
 /*-------------------------
   Testimonial CAROUSEL JS
 -------------------------*/
