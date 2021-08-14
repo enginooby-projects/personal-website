9b8fc8c5b0a930c0d27e11af17f3f02ebdf6be51
unknown <enginoobz@gmail.com>
2021-08-14 03:42:00 +0200

    [Setup] Task: live server & open file in browser

---
+++ manual-tasks/open.js
@@ -0,0 +1,5 @@
+const open = require('open');
+
+// open('http://localhost:3000/page.php');
+open('http://localhost:3000/dui/demo/index.php')
+open('http://localhost:3000/dui/tasks/convert-php-files.php');
